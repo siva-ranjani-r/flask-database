bf186462c10977580250fc0c3369425ae384187c
siva-ranjani-r <siva2300rs@gmail.com>
2023-01-06 22:44:29 +0530

    Adding python fil'

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask,render_template,redirect,request
+import sqlite3 as sql
+
+app = Flask(__name__)
+
+# @app.route("/")
+# def home():
+#     conn =sql.connect('user.db')
+#     conn.row_factory = sql.Row
+#     cur =conn.cursor()
+
+#     cur.execute("SELECT * FROM post")
+#     data =cur.fetchall()
+
+#     return render_template("index.html",data=data)
+
+
+@app.route("/",methods=['POST','GET'])
+def function():
+    id=request.form.get("scheme_code")
+    conn=sql.connect("fund.db")
+    conn.row_factory=sql.Row
+    cur=conn.cursor()
+    cur.execute("Select * from money where scheme_code=?",(id,))
+    data=cur.fetchall()
+    # cur.fetchall()
+    # l=[]
+    # for row in cur:
+    #     for field in row:
+    #         l.append(field)
+    # print(l)
+    return render_template("fund.html",data=data)
+
+
+if __name__=='__main__':
+    app.run(debug=True)
