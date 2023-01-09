2d6c139c806827fd645dec86e6502558b83cbb96
siva-ranjani-r <siva2300rs@gmail.com>
2023-01-09 09:57:47 +0530

    Adding python file

--- app.py
+++ app.py
@@ -1,30 +1,19 @@
 from flask import Flask,render_template,redirect,request
+import requests
 import sqlite3 as sql
 
 app = Flask(__name__)
 
-# @app.route("/")
-# def home():
-#     conn =sql.connect('user.db')
-#     conn.row_factory = sql.Row
-#     cur =conn.cursor()
+@app.route("/")
+def home():
+    conn =sql.connect('user.db')
+    conn.row_factory = sql.Row
+    cur =conn.cursor()
 
-#     cur.execute("SELECT * FROM post")
-#     data =cur.fetchall()
+    cur.execute("SELECT * FROM post")
+    data =cur.fetchall()
 
-#     return render_template("index.html",data=data)
-
-
-@app.route("/",methods=['POST','GET'])
-def function():
-    id=request.form.get("scheme_code")
-    conn=sql.connect("fund.db")
-    conn.row_factory=sql.Row
-    cur=conn.cursor()
-    cur.execute("Select * from money where scheme_code=?",(id,))
-    data=cur.fetchall()
-    return render_template("fund.html",data=data)
-
+    return render_template("index.html",data=data)
 
 
 
